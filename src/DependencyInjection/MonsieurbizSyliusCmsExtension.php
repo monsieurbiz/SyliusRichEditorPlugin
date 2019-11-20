fa6b292c4e299d6a9193101174bd2617231f48f2
Maxime Huran <m.huran@monsieurbiz.com>
2019-11-20 13:00:35 +0100

    Remove useless use

--- src/DependencyInjection/MonsieurbizSyliusCmsExtension.php
+++ src/DependencyInjection/MonsieurbizSyliusCmsExtension.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Monsieurbiz\SyliusCmsPlugin\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class MonsieurbizSyliusCmsExtension extends Extension
 {
