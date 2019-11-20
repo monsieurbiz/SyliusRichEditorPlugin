d3565482e3098fc39b1f1b7790dc9e5e832e9497
Maxime Huran <m.huran@monsieurbiz.com>
2019-11-20 10:34:25 +0100

    Don't require all sylius for plugin

--- src/MonsieurbizSyliusCmsPlugin.php
+++ src/MonsieurbizSyliusCmsPlugin.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace Monsieurbiz\SyliusCmsPlugin;
 
-use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Monsieurbiz\SyliusCmsPlugin\Application\MonofonyPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class MonsieurbizSyliusCmsPlugin extends Bundle
 {
-    use SyliusPluginTrait;
+    use MonofonyPluginTrait;
 }
