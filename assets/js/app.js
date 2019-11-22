1387f2f8547dfae0d5a4ef352a0cc68a0f9e66a7
Guirec Lefort <g.lefort@monsieurbiz.com>
2019-11-22 17:22:41 +0100

    WIP - Show content blocks instead of content field

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,41 @@
-console.log(monsieurbizCmsPlugin.uiElements);
+const uiElements = monsieurbizCmsPlugin.uiElements;
+const target = document.getElementById('app_page_translations_fr_FR_content');
+
+function hideField() {
+    target.setAttribute('hidden', 'true');
+}
+
+function displayElements(elements) {
+    const elementsContainer = document.createElement('div');
+
+    elementsContainer.classList.add('ui', 'segment', 'drag-list');
+
+    for (let key in elements) {
+        const element = `
+            <div class="ui segment raised drag-item" draggable="true">
+                <div class="ui grid">
+                    <div class="four wide column">
+                        <img class="ui small image" src="${elements[key].image}" alt="" width="150" height="150">
+                    </div>
+                    <div class="twelve wide column">
+                        <h2 class="ui header">${elements[key].title}</h2>
+                        <p>${elements[key].description}</p>
+                        <div class="ui buttons">
+                            <button class="ui button" type="button">Editer</button>
+                            <button class="ui button negative" type="button">Supprimer</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        elementsContainer.insertAdjacentHTML('beforeend', element);
+    }
+
+    target.parentNode.appendChild(elementsContainer);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    hideField();
+    displayElements(uiElements);
+});
