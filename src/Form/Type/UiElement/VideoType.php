6ac19c97f75fe6cb5ef83d409574cf234627db98
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2019-12-13 16:09:29 +0100

    Create form for Video UiElement

---
+++ src/Form/Type/UiElement/VideoType.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Monsieurbiz\SyliusCmsPlugin\Form\Type\UiElement;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class VideoType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('video', FileType::class, [
+                'label' => 'monsieurbiz_cmsplugin.ui_element.video.field.video',
+                'data_class' => null,
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\File([
+                        'mimeTypes' => [
+                            'video/mp4',
+                            'video/mp3'
+                        ]
+                    ])
+                ],
+            ])
+            ->add('image', FileType::class, [
+                'label' => 'monsieurbiz_cmsplugin.ui_element.video.field.image',
+                'data_class' => null,
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Image([])
+                ],
+            ])
+        ;
+    }
+}
