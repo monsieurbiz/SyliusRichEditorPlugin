7dd0d098d97f221c9f76076ea2eb3a190220c326
Maxime Huran <m.huran@monsieurbiz.com>
2020-06-04 15:24:18 +0200

    Add Taxon product UI Element with empty render

---
+++ src/Form/Type/UiElement/TaxonProductType.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusRichEditorPlugin\Form\Type\UiElement;
+
+use Sylius\Bundle\TaxonomyBundle\Form\Type\TaxonAutocompleteChoiceType;
+use Sylius\Component\Core\Model\Taxon;
+use Sylius\Component\Taxonomy\Factory\TaxonFactoryInterface;
+use Sylius\Component\Taxonomy\Repository\TaxonRepositoryInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as FormTextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class TaxonProductType extends AbstractType
+{
+    /**
+     * @var TaxonRepositoryInterface
+     */
+    protected $taxonRepository;
+    /**
+     * @var TaxonFactoryInterface
+     */
+    protected $taxonFactory;
+
+    public function __construct(TaxonRepositoryInterface $taxonRepository, TaxonFactoryInterface $taxonFactory)
+    {
+        $this->taxonRepository = $taxonRepository;
+        $this->taxonFactory = $taxonFactory;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('taxon', TaxonAutocompleteChoiceType::class, [
+                'required' => true,
+                'label' => 'monsieurbiz_richeditor_plugin.ui_element.taxon_product.field.taxon',
+                'constraints' => [
+                    new Assert\NotBlank([])
+                ],
+            ])
+            ->add('number_of_products', FormTextType::class, [
+                'required' => true,
+                'label' => 'monsieurbiz_richeditor_plugin.ui_element.taxon_product.field.number_of_products',
+                'constraints' => [
+                    new Assert\NotBlank([]),
+                    new Assert\GreaterThan(0),
+                ],
+            ])
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $data = $event->getData();
+                $taxonCode = $data['taxon'] ?? '';
+                if ($taxonCode) {
+                    $data['taxon'] = $this->taxonRepository->findOneBy(['code' => $taxonCode]);
+                } else {
+                    $data['taxon'] = $this->taxonFactory->createNew();
+                }
+                $event->setData($data);
+            })
+        ;
+        $builder->resetModelTransformers();
+    }
+}
---
+++ src/UiElement/Element/TaxonProduct.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusRichEditorPlugin\UiElement\Element;
+
+use MonsieurBiz\SyliusRichEditorPlugin\Form\Type\UiElement\TaxonProductType;
+use MonsieurBiz\SyliusRichEditorPlugin\UiElement\AbstractUiElement;
+
+class TaxonProduct extends AbstractUiElement
+{
+    protected $type = 'taxon_product';
+
+    public function getFields(): array
+    {
+        return ['taxon', 'number_of_products'];
+    }
+
+    public function getFormClass(): string
+    {
+        return TaxonProductType::class;
+    }
+}
