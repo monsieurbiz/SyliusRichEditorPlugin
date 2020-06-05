edb6b192d09b4528cfb9dfeda91e7a960f60bc7b
Maxime Huran <m.huran@monsieurbiz.com>
2020-06-05 19:19:59 +0200

    Add button link element

---
+++ src/Form/Type/UiElement/ButtonLinkType.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusRichEditorPlugin\Form\Type\UiElement;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as FormTextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ButtonLinkType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('label', FormTextType::class, [
+                'required' => true,
+                'label' => 'monsieurbiz_richeditor_plugin.ui_element.button.field.label',
+                'constraints' => [
+                    new Assert\NotBlank([])
+                ],
+            ])
+            ->add('link', FormTextType::class, [
+                'required' => true,
+                'label' => 'monsieurbiz_richeditor_plugin.ui_element.button.field.link',
+                'constraints' => [
+                    new Assert\Url([]),
+                    new Assert\NotBlank([])
+                ],
+            ])
+        ;
+    }
+}
