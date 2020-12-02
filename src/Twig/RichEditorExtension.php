036725ee4155ac3b1d24d56f6ef97f5ee5d1898b
Roshyo <stefdecock68@gmail.com>
2020-12-02 14:23:25 +0100

    Fix refactor gone wrong (thanks phpstan)

--- src/Twig/RichEditorExtension.php
+++ src/Twig/RichEditorExtension.php
@@ -114,6 +114,7 @@ final class RichEditorExtension extends AbstractExtension
             // If the JSON decode failed, return a new UIElement with default configuration
             return [
                 'type' => $this->getDefaultElement(),
+                'data' => $content,
             ];
         }
 
