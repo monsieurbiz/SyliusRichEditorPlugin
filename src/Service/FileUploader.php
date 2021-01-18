95503adb7e13bd643fd97ec2f3e6e2e8b15aad56
Prou Yann <toofff.development@gmail.com>
2021-01-18 11:37:32 +0100

    Fix: PHPCS

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -50,7 +50,7 @@ class FileUploader
     public function upload(UploadedFile $file)
     {
         // See @https://symfony.com/doc/4.4/controller/upload_file.html
-        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $originalFilename = pathinfo($file->getClientOriginalName(), \PATHINFO_FILENAME);
         $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
         $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
 
