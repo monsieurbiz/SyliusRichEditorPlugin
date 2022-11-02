e99307b7e81d1f86ea76d36b73d15e82bea1cac0
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2022-11-02 14:23:53 +0100

    feat: gives possibility to define specific const for files

--- src/Form/Constraints/RichEditorConstraints.php
+++ src/Form/Constraints/RichEditorConstraints.php
@@ -21,37 +21,37 @@ final class RichEditorConstraints
      * Return constraint depending on data
      * If user created the element, the field is required
      * If it's an edition and it contains a filename, we don't flag it as required.
-     *
-     * @return array
      */
-    public static function getImageConstraints(array $data, string $fieldName, bool $required = true)
+    public static function getImageConstraints(array $data, string $fieldName, bool $required = true, array $defaultConstraints = []): array
     {
-        // No constraint if current value is a string with the filepath
-        if (isset($data[$fieldName]) && \is_string($data[$fieldName])) {
-            return [];
-        }
-
-        if (!$required) {
-            return [
+        if (empty($defaultConstraints)) {
+            $defaultConstraints = [
                 new Assert\Image([]),
             ];
         }
 
-        // No image set yet, we require file
-        return [
-            new Assert\NotBlank([]),
-            new Assert\Image([]),
-        ];
+        return self::getFileConstraints($data, $fieldName, $required, $defaultConstraints);
     }
 
     /**
      * Return constraint depending on data
      * If user created the element, the field is required
      * If it's an edition and it contains a filename, we don't flag it as required.
-     *
-     * @return array
      */
-    public static function getVideoConstraints(array $data, string $fieldName, bool $required = true)
+    public static function getVideoConstraints(array $data, string $fieldName, bool $required = true, array $defaultConstraints = []): array
+    {
+        if (empty($defaultConstraints)) {
+            $defaultConstraints = [
+                new Assert\File([
+                    'mimeTypes' => ['video/mpeg', 'video/mp4', 'video/quicktime', 'video/x-ms-wmv', 'video/x-msvideo', 'video/x-flv'],
+                ]),
+            ];
+        }
+
+        return self::getFileConstraints($data, $fieldName, $required, $defaultConstraints);
+    }
+
+    private static function getFileConstraints(array $data, string $fieldName, bool $required, array $constraints): array
     {
         // No constraint if current value is a string with the filepath
         if (isset($data[$fieldName]) && \is_string($data[$fieldName])) {
@@ -59,19 +59,12 @@ final class RichEditorConstraints
         }
 
         if (!$required) {
-            return [
-                new Assert\File([
-                    'mimeTypes' => ['video/mpeg', 'video/mp4', 'video/quicktime', 'video/x-ms-wmv', 'video/x-msvideo', 'video/x-flv'],
-                ]),
-            ];
+            return $constraints;
         }
 
-        // No video set yet, we require file
-        return [
-            new Assert\NotBlank([]),
-            new Assert\File([
-                'mimeTypes' => ['video/mpeg', 'video/mp4', 'video/quicktime', 'video/x-ms-wmv', 'video/x-msvideo', 'video/x-flv'],
-            ]),
-        ];
+        // No file set yet, we require file
+        $constraints[] = new Assert\NotBlank([]);
+
+        return $constraints;
     }
 }
