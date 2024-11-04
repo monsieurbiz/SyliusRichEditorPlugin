93391809070e85b17aeb8b8dc9419c01962b4067
Mathieu Santostefano <msantostefano@protonmail.com>
2024-11-04 11:03:22 +0100

    feat(separator): Add style property on Separator UiElement

--- src/Form/Type/UiElement/SeparatorType.php
+++ src/Form/Type/UiElement/SeparatorType.php
@@ -15,12 +15,19 @@ namespace MonsieurBiz\SyliusRichEditorPlugin\Form\Type\UiElement;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 
 class SeparatorType extends AbstractType
 {
+    public const SOLID_STYLE = 'solid';
+
+    public const DASHED_STYLE = 'dashed';
+
+    public const DOTTED_STYLE = 'dotted';
+
     /**
      * @inheritdoc
      */
@@ -30,6 +37,14 @@ class SeparatorType extends AbstractType
             ->add('hidden', CheckboxType::class, [
                 'label' => 'monsieurbiz_richeditor_plugin.ui_element.monsieurbiz.separator.field.hidden',
                 'required' => false,
+            ])
+            ->add('style', ChoiceType::class, [
+                'label' => 'monsieurbiz_richeditor_plugin.ui_element.monsieurbiz.separator.field.style.label',
+                'choices' => [
+                    'monsieurbiz_richeditor_plugin.ui_element.monsieurbiz.separator.field.style.choices.solid' => self::SOLID_STYLE,
+                    'monsieurbiz_richeditor_plugin.ui_element.monsieurbiz.separator.field.style.choices.dashed' => self::DASHED_STYLE,
+                    'monsieurbiz_richeditor_plugin.ui_element.monsieurbiz.separator.field.style.choices.dotted' => self::DOTTED_STYLE,
+                ],
             ])
             ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event): void {
                 /** @var array $data */
