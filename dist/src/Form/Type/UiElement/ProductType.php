8c5046d87fb7ad6cd03ded17cdb764a1f9626900
Maxime Huran <m.huran@monsieurbiz.com>
2025-03-21 15:46:49 +0100

    feat(debug): add product ui element for debug purposes

---
+++ dist/src/Form/Type/UiElement/ProductType.php
@@ -0,0 +1,59 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Rich Editor plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Form\Type\UiElement;
+
+use App\UiElement\ProductUiElement;
+use MonsieurBiz\SyliusRichEditorPlugin\Attribute\AsUiElement;
+use Sylius\Bundle\AdminBundle\Form\Type\ProductAutocompleteType;
+use Sylius\Bundle\ResourceBundle\Form\DataTransformer\ResourceToIdentifierTransformer;
+use Sylius\Resource\Doctrine\Persistence\RepositoryInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\ReversedTransformer;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[AsUiElement(
+    code: 'app.product',
+    icon: 'gg:debug',
+    uiElement: ProductUiElement::class,
+    tags: ['debug'],
+)]
+class ProductType extends AbstractType
+{
+    /** @param RepositoryInterface<ProductInterface> $productRepository */
+    public function __construct(private RepositoryInterface $productRepository)
+    {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => 'app.ui_element.product.field.title',
+                'required' => false,
+            ])
+            ->add('product_code', ProductAutocompleteType::class, [
+                'label' => 'app.ui_element.product.field.product',
+                'required' => true,
+                'constraints' => [
+                    new Assert\NotBlank([]),
+                ],
+            ])
+            ->get('product_code')->addModelTransformer(
+                new ReversedTransformer(new ResourceToIdentifierTransformer($this->productRepository, 'code')),
+            )
+        ;
+    }
+}
