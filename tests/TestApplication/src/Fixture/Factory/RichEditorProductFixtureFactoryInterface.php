b9044616267f6bd926ddf7b159f3d4019f1b5e32
Maxime Leclercq <m.leclercq@monsieurbiz.com>
2025-06-18 18:08:25 +0200

    feat: use the sylius test application

---
+++ tests/TestApplication/src/Fixture/Factory/RichEditorProductFixtureFactoryInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Rich Editor plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE.txt
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\MonsieurBiz\SyliusRichEditorPlugin\TestApplication\Fixture\Factory;
+
+use Sylius\Bundle\CoreBundle\Fixture\Factory\ExampleFactoryInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+interface RichEditorProductFixtureFactoryInterface extends ExampleFactoryInterface
+{
+    public function getOptionsResolver(): OptionsResolver;
+}
